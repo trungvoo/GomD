f665e8e802e32f61808403898b9650b6c5fe9a07
trungvo_gomicorp <trungvo@gomicorp.vn>
2023-10-04 10:49:24 +0700

    custom animation web

--- XD/Project/main.js
+++ XD/Project/main.js
@@ -17,3 +17,26 @@ new Swiper('.swiper-member', {
         }
     }
 });
+
+
+const animatedEls = document.querySelectorAll("[data-animation]");
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		const animation = entry.target.getAttribute("data-animation");
+
+		if (entry.isIntersecting) {
+			entry.target.classList.add("animated", `${animation}`);
+		} 
+        // else {
+		// 	entry.target.classList.remove("animated", `${animation}`);
+		// }
+	});
+});
+
+animatedEls.forEach((el) => observer.observe(el));
+
+
+$(window).on('load', function () {
+    console.log("true")
+});
