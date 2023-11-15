db65a657681d9a84a1f085b229e55644857fd9a8
trungvo_gomicorp <trungvo@gomicorp.vn>
2023-11-15 11:10:31 +0700

    media slide

--- XD/Project/main.js
+++ XD/Project/main.js
@@ -5,8 +5,8 @@ new Swiper('.swiper-member', {
     //     disableOnInteraction: false,
     // },
     navigation: {
-        nextEl: '.swiper-btn-next',
-        prevEl: '.swiper-btn-prev',
+        nextEl: '.section-video-1-content-wrap .swiper-btn-next',
+        prevEl: '.section-video-1-content-wrap .swiper-btn-prev',
     },
      breakpoints: {
         0: {
@@ -17,6 +17,30 @@ new Swiper('.swiper-member', {
         }
     }
 });
+
+new Swiper('.swiper-media-1', {
+    spaceBetween: 0,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+     breakpoints: {
+        0: {
+            slidesPerView: 2.2
+        },
+        768: {
+            slidesPerView: 4
+        }
+    }
+});
+
 
 
 const animatedEls = document.querySelectorAll("[data-animation]");
@@ -39,4 +63,17 @@ animatedEls.forEach((el) => observer.observe(el));
 
 $(window).on('load', function () {
     console.log("true")
+});
+
+
+$(".swiper-slide--items").on('click', function(){
+    var boolHasMedia = $(this).data("media-has");
+    if(boolHasMedia == true){
+        var srcMedia = $(this).data("media-src");
+        if($("#embed-main-media").attr("src") != srcMedia){
+            var widthMedia = $(this).data("media-width");
+            var heightMedia = $(this).data("media-height");
+            $("#embed-main-media").attr("src", srcMedia);
+        }
+    }
 });
