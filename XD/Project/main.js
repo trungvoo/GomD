e6829d887abeffce70ea76ef9f6af52d7a583fdb
trungvo_gomicorp <trungvo@gomicorp.vn>
2023-11-17 18:58:02 +0700

    custom button section 3, create section new

--- XD/Project/main.js
+++ XD/Project/main.js
@@ -42,6 +42,29 @@ new Swiper('.swiper-media-1', {
 });
 
 
+new Swiper('.swiper-news', {
+    spaceBetween: 0,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2.2
+        },
+        768: {
+            slidesPerView: 4
+        }
+    }
+});
+
 
 const animatedEls = document.querySelectorAll("[data-animation]");
 
