212559bd8401a72190643c66f4a483b820f1310a
trungvo_gomicorp <trungvo@gomicorp.vn>
2024-03-26 09:13:29 +0700

    create Gomd 3.0

---
+++ XD/Gomd3.0/main-animation.js
@@ -0,0 +1,21 @@
+gsap.fromTo('.loading-page',
+ {opacity: 1},
+ {
+    opacity: 0,
+    duration: 1.5,
+    delay: 3.5
+
+})
+
+gsap.fromTo('.name-page-title',
+ {
+    y: 50,
+    opacity: 0
+    },
+ {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    delay: 0.5
+
+})
