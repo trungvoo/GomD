0751039ba3ed1808dd6649b00ac236ee0568e8db
trungvo_gomicorp <trungvo@gomicorp.vn>
2024-04-10 09:30:02 +0700

    created partners

--- XD/Gomd3.0/Content/Script/main.js
+++ XD/Gomd3.0/Content/Script/main.js
@@ -1,6 +1,7 @@
 $(function () {
     toggleLanguage();
     swiperGomdX();
+    swiperGomdClip();
 });
 
 
@@ -44,13 +45,60 @@ function swiperGomdX() {
 
             },
             768: {
+                slidesPerView: 3,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 50,
+                    modifier: 3,
+                    scale: 1,
+                    slideShadows: true
+                },
+            }
+        }
+    });
+};
+
+// Swiper Gomd Clip
+function swiperGomdClip() {
+    new Swiper('.swiper-gomdClip', {
+        spaceBetween: 0,
+        // autoplay: {
+        //     delay: 2500,
+        //     disableOnInteraction: false,
+        // },
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-gomdClip-btn-next',
+            prevEl: '.swiper-gomdClip-btn-prev',
+        },
+        loop: true,
+        // autoplay: {
+        //     delay: 3000,
+        // },
+        breakpoints: {
+            0: {
                 slidesPerView: 2,
                 coverflowEffect: {
                     rotate: 0,
                     stretch: 0,
-                    depth: 180,
+                    depth: 148,
                     modifier: 4,
                     scale: 1.05,
+                    slideShadows: true
+                }
+
+            },
+            768: {
+                slidesPerView: 3,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 50,
+                    modifier: 3,
+                    scale: 1,
                     slideShadows: true
                 },
             }
